4e2b6c423ace517ffb6d850ccbc5bbbbde3d8f56
itemhsu <itemhsu@gmail.com>
2021-04-22 21:25:00 +0800

    Add files via upload
    using "python gen.py" to gen the raceline

---
+++ gen.py
@@ -0,0 +1,130 @@
+import copy
+import os
+import numpy as np
+from datetime import datetime
+from shapely.geometry import Point, Polygon
+import matplotlib.pyplot as plt
+
+# Number of times to iterate each new race line point
+# keep this at 3-8 for best balance of performance and desired result
+XI_ITERATIONS=8 # default 4
+
+# Number of times to scan the entire race track to iterate
+# 500 will get a good start, 1500 will be closer to optimal result
+LINE_ITERATIONS=10000 # default 1000
+TRACK_NAME = 'reInvent2019_track'
+
+os.system("wget 'https://github.com/aws-deepracer-community/deepracer-race-data/raw/main/raw_data/tracks/npy/%s.npy' -O %s.npy" % (TRACK_NAME ,TRACK_NAME))
+waypoints = np.load("%s.npy" % TRACK_NAME)
+center_line = waypoints[:,0:2]
+inner_border = waypoints[:,2:4]
+outer_border = waypoints[:,4:6]
+
+def menger_curvature(pt1, pt2, pt3, atol=1e-3):
+
+    vec21 = np.array([pt1[0]-pt2[0], pt1[1]-pt2[1]])
+    vec23 = np.array([pt3[0]-pt2[0], pt3[1]-pt2[1]])
+
+    norm21 = np.linalg.norm(vec21)
+    norm23 = np.linalg.norm(vec23)
+
+    theta = np.arccos(np.dot(vec21, vec23)/(norm21*norm23))
+    if np.isclose(theta-np.pi, 0.0, atol=atol):
+        theta = 0.0
+
+    dist13 = np.linalg.norm(vec21-vec23)
+
+    return 2*np.sin(theta) / dist13
+
+def improve_race_line(old_line, inner_border, outer_border):
+    '''Use gradient descent, inspired by K1999, to find the racing line'''
+    # start with the center line
+    new_line = copy.deepcopy(old_line)
+    ls_inner_border = Polygon(inner_border)
+    ls_outer_border = Polygon(outer_border)
+    for i in range(0,len(new_line)):
+        xi = new_line[i]
+        npoints = len(new_line)
+        prevprev = (i - 2 + npoints) % npoints
+        prev = (i - 1 + npoints) % npoints
+        nexxt = (i + 1 + npoints) % npoints
+        nexxtnexxt = (i + 2 + npoints) % npoints
+        #print("%d: %d %d %d %d %d" % (npoints, prevprev, prev, i, nexxt, nexxtnexxt))
+        ci = menger_curvature(new_line[prev], xi, new_line[nexxt])
+        c1 = menger_curvature(new_line[prevprev], new_line[prev], xi)
+        c2 = menger_curvature(xi, new_line[nexxt], new_line[nexxtnexxt])
+        target_ci = (c1 + c2) / 2
+        #@if target_ci > 1.1:
+        #    target_ci=1.1
+        #    print("i %d ci %f target_ci %f c1 %f c2 %f" % (i, ci, target_ci, c1, c2))
+
+        # Calculate prospective new track position, start at half-way (curvature zero)
+        xi_bound1 = copy.deepcopy(xi)
+        xi_bound2 = ((new_line[nexxt][0] + new_line[prev][0]) / 2.0, (new_line[nexxt][1] + new_line[prev][1]) / 2.0)
+        p_xi = copy.deepcopy(xi)
+        for j in range(0,XI_ITERATIONS):
+            p_ci = menger_curvature(new_line[prev], p_xi, new_line[nexxt])
+            #print("i: {} iter {} p_ci {} p_xi {} b1 {} b2 {}".format(i,j,p_ci,p_xi,xi_bound1, xi_bound2))
+            if np.isclose(p_ci, target_ci):
+                break
+            if p_ci < target_ci:
+                # too flat, shrinking track too much
+                #print("too flat")
+            
+                xi_bound2 = copy.deepcopy(p_xi)
+                new_p_xi = ((xi_bound1[0] + p_xi[0]) / 2.0, (xi_bound1[1] + p_xi[1]) / 2.0)
+                if Point(new_p_xi).within(ls_inner_border) or not Point(new_p_xi).within(ls_outer_border):
+                    xi_bound1 = copy.deepcopy(new_p_xi)
+                else:
+                    p_xi = new_p_xi
+            else:
+                #print("too curved")
+                # too curved, flatten it out
+                xi_bound1 = copy.deepcopy(p_xi)
+                new_p_xi = ((xi_bound2[0] + p_xi[0]) / 2.0, (xi_bound2[1] + p_xi[1]) / 2.0)
+
+                # If iteration pushes the point beyond the border of the track,
+                # just abandon the refinement at this point.  As adjacent
+                # points are adjusted within the track the point should gradually
+                # make its way to a new position.  A better way would be to use
+                # a projection of the point on the border as the new bound.  Later.
+                if Point(new_p_xi).within(ls_inner_border) or not Point(new_p_xi).within(ls_outer_border):
+                    xi_bound2 = copy.deepcopy(new_p_xi)
+                else:
+                    p_xi = new_p_xi
+        new_xi = p_xi
+        # New point which has mid-curvature of prev and next points but may be outside of track
+        #print((new_line[i], new_xi))
+        new_line[i] = new_xi
+    return new_line
+
+
+# start along centerline of track
+#race_line = copy.deepcopy(center_line[:-1])  # Use this for centerline being outer bound
+race_line = copy.deepcopy(outer_border[:-1])
+for i in range(LINE_ITERATIONS):
+    race_line = improve_race_line(race_line, inner_border, outer_border)  # Remove "_new" for entire track width
+    if i % 20 == 0: print("Iteration %d" % i, end="\r")
+
+now = datetime.now()
+prefix = './%s-%d-%d-%s' % (TRACK_NAME, LINE_ITERATIONS, XI_ITERATIONS, now.strftime('%Y-%m-%d-%H%M%S'))
+py_fname = prefix + '.py'
+npy_fname = prefix + '.npy'
+with open(py_fname, "w+") as file:
+    print("Writing python code to %s" % py_fname)
+    file.write(np.array_repr(race_line))
+    
+print("Writing numpy binary to %s" % npy_fname)
+np.save(npy_fname, race_line)
+
+def plot_points(ax, points):
+    ax.scatter(points[:-1,0], points[:-1,1], s=1)
+    for i,p in enumerate(points):
+        ax.annotate(i, (p[0], p[1]))
+# Plot 
+fig, ax = plt.subplots(figsize=(12,9))
+plot_points(ax, race_line)
+plot_points(ax, waypoints[:-1,2:4])
+plot_points(ax, waypoints[:-1,4:6])
+ax.axis('equal')
+plt.savefig('%s.png' %TRACK_NAME)
